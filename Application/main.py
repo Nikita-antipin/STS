63ab0d6c80c93e599f72252865e0666fc5f62e00
Nikita-antipin <78693495+Nikita-antipin@users.noreply.github.com>
2022-07-14 01:32:47 +0300

    14.07.22

--- Application/main.py
+++ Application/main.py
@@ -1,18 +1,41 @@
-from time import sleep
 from tkinter import *
 from paste_from_clipboard import *
-from get_processed_subs import get_processed_subs
-from translate_subs import *
+from get_processed_sub import get_processed_sub
+from translate_sub import *
+import os
 
-from datetime import datetime
-text_to_translate = str(get_clipboard_text())[2:len(str(get_clipboard_text()))-1]
+#That function saves pathname to sub
+def save_path_in_file(text):
+    path_to_script = os.path.dirname(os.path.abspath(__file__))
+    my_filename = os.path.join(path_to_script, "file_where_i_save_path.txt")
+    if os.path.exists(my_filename):
+        file1 = open(my_filename, "w")
+        file1.write(text)
+        file1.close()
+    else:
+        file1 = open(my_filename, "w")
+        file1.write(text)
+        file1.close()
 
+#Creates new window but now isn't used
+def openNewWindow():
+    #gets a path to file, close the new window, starts sub play
+    def get_input():
+        #click_button_to_continue()
+        time_with_text2 = get_processed_sub()
+        text = my_text_box.get("1.0", "end-1c")
+        #save_it_in_file(text)
+        newWindow.destroy()
 
-root = Tk()
-root['bg'] = '#fafafa'
-root.title('STS - SubsToStudy')
-root.geometry("700x180")
-time_with_text = get_processed_subs()
+#Gui newWindow
+    newWindow = Toplevel(root)
+    newWindow.title("Settings")
+    newWindow.geometry("700x100")
+    btn_to_continue = Button(newWindow, text="Continue", command=get_input)
+    my_text_box = Text(newWindow, width=86, height=2)
+    my_text_box.pack()
+    Label(newWindow,text="But before place Absolute Path to your subtitles here").pack()
+    btn_to_continue.pack()
 
 
 def click_button_to_translate():
@@ -22,52 +45,71 @@ def click_button_to_translate():
     translated_output.insert(END, translated_copied_text + "\n")
     translated_output.update()
 
-
 def click_button_to_start():
-    global text_to_translate
-    print(text_to_translate)
-    if str(get_clipboard_text())[2:len(str(get_clipboard_text())) - 1] == "4":
 
+    time_with_text2 = get_processed_sub()
+    text = output.get("1.0", "end-1c")
+    if text != "Pls, write absolute path to sub or just press start to continue with last one":
+        save_path_in_file(text)
+    else:
+        save_path_in_file(r"C:\Users\Никита\PycharmProjects\pythonProject\dj.sub")
+    continuation()
+#works after button is pressed
+def continuation():
+    global text_to_translate
+
+    #partition that is used to pinpoint the case when the button is pressed in the browser
+    if str(get_clipboard_text()) == "4323232":
         click_button_to_translate()
         while True:
-
-            if str(get_clipboard_text())[2:len(str(get_clipboard_text())) - 1] == "345489494":
+            if str(get_clipboard_text()) == "345489494":
                 break
-
-    if str(get_clipboard_text())[2:len(str(get_clipboard_text()))-1] == "45478745548":
-
+    if str(get_clipboard_text()) == "45478745548":
         while True:
-
-            if str(get_clipboard_text())[2:len(str(get_clipboard_text()))-1] == "345489494":
+            if str(get_clipboard_text()) == "345489494":
                 break
+
     output.delete('1.0', END)
     output.insert(END, time_with_text[0][1])
     output.update()
-    text_to_translate = str(get_clipboard_text())[2:len(str(get_clipboard_text())) - 1]
 
-
+    text_to_translate = str(get_clipboard_text())
+    #Sub part
     if len(time_with_text) - 1 != 0:
+        #time between pair of sub
         dt = time_with_text[1][0] - time_with_text[0][0]
     else:
         dt = 0
     time_with_text.pop(0)
+    #Start this function again after some time
+    root.after(int(dt*1000), continuation)
 
-    root.after(int(dt*1000), click_button_to_start)
 
+#GUI partition
 
-
+#Let's create a main window
+root = Tk()
+#gives the window some properties(color: white, name: 'STS - SubToStudy', size of window:"700x180")
+root['bg'] = '#fafafa'
+root.title('STS - SubToStudy')
+root.geometry("700x160")
+#The first window, where subtitles appear
 output = Text(root, width=86, height=3)
-translated_output = Text(root, width=86, height=3)
-a = "Pls, place it in the most suitable place and press start"
-output.insert(END, a)
+output.insert(END, "Pls, write absolute path to sub or just press start to continue with last one")
 output.grid(row=5, column=1, columnspan=2, padx=5, pady=(0,10))
-translated_output.insert(END,"translated text will be appear here")
-translated_output.grid(row=7, column=1, columnspan=2, padx=5, pady=(0,10))
+#The Second window, where translated sub appears
+translated_output = Text(root, width=86, height=3)
+translated_output.insert(END,"To translate unknown words from sub you need to copy them and press translate in your browser. After that this app and video will stop until you press button start in the Browser")
+translated_output.grid(row=6, column=1, columnspan=2, padx=5, pady=(0,10))
+#Button
 btn_to_start = Button(root, text="Start", command=click_button_to_start)
-#btn_to_translate = Button(root, text="Translate", command=click_button_to_translate)
-btn_to_start.grid(row=6, column=1, columnspan=2, padx=5, pady=(0,10))
-#btn_to_translate.grid(row=6, column=2, columnspan=2, padx=5, pady=(0,10))
+btn_to_start.grid(row=7, column=1, columnspan=2, padx=5, pady=(0,10))
+
+
+#Logic partition
+
+text_to_translate = str(get_clipboard_text())
+#data that is like [(time, subtitle),....]
+time_with_text = get_processed_sub()
 
 root.mainloop()
-
-
--- Application/paste_from_clipboard.py
+++ Application/paste_from_clipboard.py
@@ -1,23 +1,12 @@
-import ctypes
-CF_TEXT = 1
+import win32clipboard
 
-kernel32 = ctypes.windll.kernel32
-kernel32.GlobalLock.argtypes = [ctypes.c_void_p]
-kernel32.GlobalLock.restype = ctypes.c_void_p
-kernel32.GlobalUnlock.argtypes = [ctypes.c_void_p]
-user32 = ctypes.windll.user32
-user32.GetClipboardData.restype = ctypes.c_void_p
-
+#just get text from clipboard
 def get_clipboard_text():
-    user32.OpenClipboard(0)
     try:
-        if user32.IsClipboardFormatAvailable(CF_TEXT):
-            data = user32.GetClipboardData(CF_TEXT)
-            data_locked = kernel32.GlobalLock(data)
-            text = ctypes.c_char_p(data_locked)
-            value = text.value
-            kernel32.GlobalUnlock(data_locked)
-            return value
-    finally:
-        user32.CloseClipboard()
+        win32clipboard.OpenClipboard()
+        data = win32clipboard.GetClipboardData()
+        win32clipboard.CloseClipboard()
+        return data
+    except:
+        return None
 
