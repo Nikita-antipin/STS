d3f613565d9658dd00d282778b327697034df71d
Nikita-antipin <78693495+Nikita-antipin@users.noreply.github.com>
2022-07-14 10:43:58 +0300

    version 3

--- Application/set_and_get_data_from_clipboard.py
+++ Application/set_and_get_data_from_clipboard.py
@@ -10,3 +10,11 @@ def get_clipboard_text():
     except:
         return None
 
+# set clipboard data
+def set_clipboard_text(text):
+    win32clipboard.OpenClipboard()
+    win32clipboard.EmptyClipboard()
+    win32clipboard.SetClipboardText(text)
+    win32clipboard.CloseClipboard()
+
+
